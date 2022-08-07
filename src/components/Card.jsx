801b24bc306b729856e50bccc282ff989d410862
Erick Saavedra <esvdrah@gmail.com>
2022-08-06 19:18:01 -0500

    Add git url project to Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,15 @@ const Card = ({ info }) => {
             {info.full_name}
           </h5>
         </a>
+
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
           {info.description}
         </p>
+
+        <p className="mb-3 font-bold text-gray-700 dark:text-gray-400">
+          Clone: {info.git_url}
+        </p>
+
         <a
           href={info.html_url}
           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
