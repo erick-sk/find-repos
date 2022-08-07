e8302a0b21caa7696413649db32164fe8d0fa8f0
Erick Saavedra <esvdrah@gmail.com>
2022-08-06 19:15:37 -0500

    Add Search component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = ({ info }) => {
   return (
     <>
       <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 transition hover:scale-105">
-        <a href={info.html_url}>
+        <a href={info.html_url} target="_blank" rel="noopener noreferrer">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {info.full_name}
           </h5>
@@ -13,6 +13,8 @@ const Card = ({ info }) => {
         <a
           href={info.html_url}
           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Read more
           <svg
